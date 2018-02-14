072811e14ef6fc916966b1a767cc64df45c3fc50
Caleb Hearon <caleb@chearon.net>
2018-02-14 10:26:20 -0500

    switch from cssuaron to css-select
    It seems like a more mature implementation/API and has been updated more
    recently

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cssauron = require('cssauron');
+const CSSselect = require('css-select');
 const Vue = require('vue');
 
 function vNodeClass(node) {
@@ -25,21 +25,67 @@ function ndata(n) {
   return Object.assign({attrs: {}, style: {}, class: {}, domProps: {}}, n && n.data);
 }
 
-const SelectorFactory = cssauron({
-  tag: 'tag',
-  class: node => vNodeClass(node),
-  id: node => node.data && node.data.attrs && node.data.attrs.id,
-  children: 'children',
-  parent: '__vnode_parent__',
-  contents: 'text',
-  attr(node, attr) {
-    if (node.data) {
-      const {attrs = {}, props = {}} = node.data;
-      if (attrs[attr]) return attrs[attr];
-      if (props[attr]) return props[attr];
-    }
+function recurse(node, filter, onMatch, parent) {
+  if (parent && filter(node)) onMatch(node, parent); // never onMatch(rootNode)
+  if (node.children && node.children.length) {
+    node.children.forEach(n => recurse(n, filter, onMatch, node));
   }
-});
+}
+
+const adapter = {
+  isTag(node) {
+    return !!node.tag;
+  },
+  existsOne(test, nodes) {
+    return nodes.some(node => {
+      const children = (node.children || []).map(adapter.isTag);
+      return test(node) || adapter.existsOne(test, children);
+    });
+  },
+  getAttributeValue(node, attr) {
+    if (attr === "class") return vNodeClass(node);
+    return node.data && node.data.attrs && node.data.attrs[attr];
+  },
+  getChildren(node) {
+    return (node.children || []).filter(adapter.isTag);
+  },
+  getName(node) {
+    return node.tag;
+  },
+  getParent(node) {
+    return node.__vnode_parent__;
+  },
+  getSiblings(node) {
+    return node.__vnode_parent__ && node.__vnode_parent__.children || [];
+  },
+  getText(node) {
+    return node.text;
+  },
+  hasAttrib(node, attr) {
+    return node.data && node.data.attrs && (attr in node.data.attrs);
+  },
+  removeSubsets(nodes) {
+    const seen = new Map();
+    return nodes.filter(node => {
+      for (let n = node; n; n = n.__vnode_parent__) {
+        if (seen.has(n)) return false;
+      }
+      return seen.set(node, true);
+    });
+  },
+  findAll(test, nodes) {
+    const results = [];
+    nodes.forEach(node => recurse(node, () => true, node => {
+      if (adapter.isTag(node) && test(node)) results.push(node);
+    }, true));
+    return results;
+  },
+  findOne(test, nodes) {
+    nodes.forEach(node => recurse(node, () => true, node => {
+      if (adapter.isTag(node) && test(node)) return node;
+    }, true));
+  }
+};
 
 const Query = function (root) {
   const {splice, push, reduce, some} = Array.prototype;
@@ -47,13 +93,6 @@ const Query = function (root) {
 
   if (isVue(root)) root = root._vnode;
   if (root) Object.assign(self, {length: 1, 0: root});
-
-  function recurse(node, filter, onMatch, parent) {
-    if (parent && filter(node)) onMatch(node, parent); // never onMatch(rootNode)
-    if (node.children && node.children.length) {
-      node.children.forEach(n => recurse(n, filter, onMatch, node));
-    }
-  }
 
   function text(node) {
     let s = '';
@@ -69,10 +108,11 @@ const Query = function (root) {
   const ret = Object.create({
     // Traversal functions
     find(selector) {
-      const filter = SelectorFactory(selector);
-      splice.call(this, 0, this.length).forEach(node => {
-        recurse(node, filter, n => push.call(this, n));
-      });
+      const allChildren = splice.call(this, 0, this.length).reduce((nodes, node) => {
+        return nodes.concat(adapter.getChildren(node));
+      }, []);
+      const results = CSSselect(selector, allChildren, {adapter});
+      results.forEach(node => push.call(this, node));
       return this;
     },
     first() {
@@ -97,7 +137,7 @@ const Query = function (root) {
       return ndata(this[0]).domProps[name];
     },
     hasClass(name) {
-      return some.call(this, n => n.staticClass === name || ndata(n).class[name]);
+      return some.call(this, n => vNodeClass(n).trim().split(/\s+/).includes(name));
     }
   });
 
